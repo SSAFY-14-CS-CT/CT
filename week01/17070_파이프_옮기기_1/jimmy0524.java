5fcd1611c04fd61cb6ab4e977f5724afa08b83e7
jimmy0524 <10jmin04@naver.com>
2025-07-31 16:39:35 +0900

    [Add] 17070. 파이프 옮기기 1

---
+++ week01/17070_파이프_옮기기_1/jimmy0524.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    // 54~
+    static int[][] arr;
+    static int answer;
+    static int N;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dfs(0, 1, 0);
+        System.out.println(answer);
+    }
+
+    // state = 0 -> 가로 1 -> 세로 2 -> 대각선
+    public static void dfs(int x, int y, int state) {
+        if (x == N - 1 && y == N - 1) {
+            answer++;
+            return;
+        }
+        // 가로
+        if (state == 0) {
+            if (y <= N - 2 && arr[x][y + 1] == 0) {
+                dfs(x, y + 1, 0);
+                if (x <= N - 2) {
+                    if (arr[x + 1][y + 1] == 0 && arr[x + 1][y] == 0) {
+                        dfs(x + 1, y + 1, 2);
+                    }
+                }
+            }
+        }
+        // 세로
+        else if (state == 1) {
+            if (x <= N - 2 && arr[x + 1][y] == 0) {
+                dfs(x + 1, y, 1);
+                if (y <= N - 2) {
+                    if (arr[x + 1][y + 1] == 0 && arr[x][y + 1] == 0) {
+                        dfs(x + 1, y + 1, 2);
+                    }
+                }
+            }
+        }
+        // 대각선
+        else {
+            if (y <= N - 2 && arr[x][y + 1] == 0) {
+                dfs(x, y + 1, 0);
+            }
+            if (x <= N - 2 && arr[x + 1][y] == 0) {
+                dfs(x + 1, y, 1);
+                if (y <= N - 2) {
+                    if (arr[x + 1][y + 1] == 0 && arr[x][y + 1] == 0) {
+                        dfs(x + 1, y + 1, 2);
+                    }
+                }
+            }
+        }
+    }
+}
